ccc0c197ad0643100683d879dbf53bbdf40104aa
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-08-09 22:25:37 +0600

    Laravel Vue FULL Calender

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index(Request $request)
+    {
+        $events = Event::get(['title','start']);
+        return response()->json(["events" => $events]);
+    }
+}
